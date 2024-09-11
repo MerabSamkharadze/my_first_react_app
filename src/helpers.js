1a69124db3b88226a257b64819d98d0c73ca110f
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-09-11 21:04:32 +0400

    add logic

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,14 @@
-function choise(items) {}
+function choise(items) {
+  let idx = Math.floor(Math.random() * items.length);
+  return items[idx];
+}
 
-function remove(item, items) {}
+function remove(item, items) {
+  for (let i = 0; i < items.lengt; i++) {
+    if (items[i] === item) {
+      return [...items.slice(0, i), ...items.slice(i + 1)];
+    }
+  }
+}
 
 export { choise, remove };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import foods from "./food";
 import { choise, remove } from "./helpers";
+
+let food = choise(foods);
+
+console.log(`i'd like one ${food}, please.`);
+console.log(`Here you go:  ${food}.`);
+console.log(`Delicious! May I have another ?`);
+
+let remaining = remove(food, foods);
+
+console.log(`Sorry, it was last one, but we have another foods : ${foods}`);
+console.log("You can choose any of it!");
