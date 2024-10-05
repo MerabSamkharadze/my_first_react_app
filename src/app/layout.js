a0a2fe9b37f986af51a94de54538f6534f9d62c6
MerabSamkharadze <Samkharadzemerab@gmail.com>
2024-10-05 20:00:42 +0400

    add ...slug dir and add page.js / client.js files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
+export const metadata = {
+  title: "React App",
+  description: "Web site created with Next.js.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-        <title>React App</title>
-        <meta name="description" content="Web site created..." />
-      </head>
       <body>
         <div id="root">{children}</div>
       </body>
